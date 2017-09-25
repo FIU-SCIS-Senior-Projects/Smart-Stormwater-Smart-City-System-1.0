850593833d25933f7c40b58ab8d327e55ef082b6
Canh Vien <cvien001@fiu.edu>
2017-09-25 15:44:18 -0400

    registeracc commit

--- smart_city_front_end/JS/Controllers/regaccCtrl.js
+++ smart_city_front_end/JS/Controllers/regaccCtrl.js
@@ -1,8 +1,10 @@
 var scApp = angular.module('scApp');
 
 scApp.controller("regaccCtrl", function ($scope) {
+    $scope.userID = "";
     $scope.username = "";
     $scope.password = "";
+    $scope.confirmPassword
     $scope.contactEmail = "";
     $scope.contactNumber = "";
     $scope.gtythresh = 30;
@@ -12,7 +14,7 @@ scApp.controller("regaccCtrl", function ($scope) {
     $scope.sms = false;
 
     $scope.register = function () {
-        $scope.hasRegistered = $scope.username + ", " + $scope.password + ", " + $scope.contactEmail + ", " + $scope.contactNumber + ", " + (+$scope.gtythresh + +$scope.ytrthresh) + ", " + $scope.ccn + ", " + $scope.email + ", " + $scope.sms;
+        $scope.hasRegistered = $scope.userID + ", " + $scope.username + ", " + $scope.password + ", " + $scope.confirmPassword + ", " + $scope.contactEmail + ", " + $scope.contactNumber + ", " + (+$scope.gtythresh + +$scope.ytrthresh) + ", " + $scope.ccn + ", " + $scope.email + ", " + $scope.sms;
     }
 
 });
